1c3071b49a0614715b44856e19983ff0a42d6786
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2023-10-31 21:44:55 +0200

    refactoring Searchbar.jsx

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
 import css from './Searchbar.module.css';
 import { FcSearch } from 'react-icons/fc';
 import { IconContext } from 'react-icons';
 
-export class Searchbar extends Component {
-  Submit = event => {
+export const Searchbar = ({ onSubmit }) => {
+  const Submit = event => {
     event.preventDefault();
     const form = event.currentTarget;
     const search = form.elements.search.value.toLowerCase();
     if (search.trim() === '') return alert('Can not be empty');
-    this.props.onSubmit(search);
+    onSubmit(search);
   };
 
-  render() {
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.Submit}>
-          <button type="submit">
-            <IconContext.Provider value={{ size: 25 }}>
-              <FcSearch />
-            </IconContext.Provider>
-          </button>
-          <input type="text" placeholder="Search images" name="search" />
-        </form>
-      </header>
-    );
-  }
-}
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={Submit}>
+        <button type="submit">
+          <IconContext.Provider value={{ size: 25 }}>
+            <FcSearch />
+          </IconContext.Provider>
+        </button>
+        <input type="text" placeholder="Search images" name="search" />
+      </form>
+    </header>
+  );
+};
